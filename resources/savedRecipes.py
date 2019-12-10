d3f17ea77fe6d4b4e3de91ea7017ecfbad1cb8c7
Brad Donakowski <braddonako@gmail.com>
2019-12-10 12:36:37 -0700

    Get route is working for users

--- resources/savedRecipes.py
+++ resources/savedRecipes.py
@@ -23,3 +23,11 @@ def save_recipe():
     print(model_to_dict(savedRecipe), 'model to dict')
     savedRecipe_dict = model_to_dict(savedRecipe)
     return jsonify(data=savedRecipe_dict, status={"code": 201, "message": "Success"})
+
+#show route on saved recipe page
+@savedRecipe.route('/<id>', methods=["GET"])
+def get_saved_recipes(id):
+    print(id, 'yeeet')
+    recipe = models.SavedRecipe.get_by_id(id)
+    print(recipe.__dict__)
+    return jsonify(data=model_to_dict(recipe), status={"code": 200, "message": "Success"})
